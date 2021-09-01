215e105adfd22a7ad8eb3f4ecc677354e503d270
alan <darkexemplar2020@gmail.com>
2021-08-31 21:07:59 -0500

    completed arrow functions exercises

---
+++ Unit10_ES2015/arrow_functions/arrow_functions.js
@@ -0,0 +1,23 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+// }
+
+/* Write an ES2015 Version */
+
+const double = arr => arr.map(val => val * 2);
+
+// Replace ALL functions with arrow functions:
+
+// function squareAndFindEvens(numbers) {
+//     var squares = numbers.map(function (num) {
+//         return num ** 2;
+//     });
+//     var evens = squares.filter(function (square) {
+//         return square % 2 === 0;
+//     });
+//     return evens;
+// }
+
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
