95a18f666103a61730891fb1cba9e8ae2524ebf1
alan <darkexemplar2020@gmail.com>
2021-11-20 11:34:32 -0600

    Completed survey app with sessions

--- Unit19_Flask/flask-survey/app.py
+++ Unit19_Flask/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = 'secret'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
+# responses = []
 
 @app.route('/')
 def show_home():
@@ -15,30 +15,48 @@ def show_home():
     instructions = satisfaction_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
 
-@app.route('/start')
-def start():
+# @app.route('/start')
+# def start():
+#     return redirect('/questions/0')
+
+@app.route('/session', methods=['POST'])
+def handle_session():
+    session['responses'] = []
     return redirect('/questions/0')
+
 
 @app.route('/questions/<int:question_num>')
 def show_question(question_num):
-    if question_num != len(responses):
+    responses = session['responses']
+    q_num = len(responses)
+    print(session['responses'])
+    print(f'q_num is equal to {q_num} in questions')
+    print(f'question_num is equal to {question_num} in questions')
+    if question_num != q_num:
         flash('Invalid question')
-        return redirect(f'/questions/{len(responses)}')
-    elif len(responses) == len(satisfaction_survey.questions):
+        return redirect(f'/questions/{q_num}')
+    elif q_num == len(satisfaction_survey.questions):
         return redirect('/thanks')
     else:
         question = satisfaction_survey.questions[question_num].question
         choices = satisfaction_survey.questions[question_num].choices
         return render_template('question.html', question_number=question_num, question=question, choices=choices)
 
+
 @app.route('/answer', methods=['POST'])
 def handle_answer():
     answer = request.form['client_answer']
+    responses = session['responses']
     responses.append(answer)
-    if len(responses) == len(satisfaction_survey.questions):
+    session['responses'] = responses
+    q_num = len(responses)
+    print(session['responses'])
+    print(f'q_num is equal to {q_num} in answers')
+    if q_num == len(satisfaction_survey.questions):
         return redirect('/thanks')
     else:
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{q_num}')
+
 
 @app.route('/thanks')
 def thank():
